063943832cb849120d0fbff3988c2a1c5f7b725e
ogirians <ogi.rians3@gmail.com>
2023-05-10 22:22:10 +0700

    berhasil setting theme

--- app.vue
+++ app.vue
@@ -1,6 +1,80 @@
 <template>
   <div>
-    <!-- <NuxtWelcome /> -->
-    <v-btn class="my-5 mx-5">klik disini</v-btn>
+    <v-container fluid class="mx-0 px-0 pt-0 mb-0">
+      <v-carousel
+        :continuous="false"
+        :show-arrows="false"
+        hide-delimiter-background
+        delimiter-icon="mdi mdi-cat"
+        light
+        height="400"
+        interval = "10000"
+        class="ma-0 mt-n16"
+
+      >
+          <v-carousel-item
+            class="mt-n16"
+            v-for="(item,i) in items"
+            :key="i"
+            :src="item.src"
+            cover
+          ></v-carousel-item>
+    </v-carousel>
+    </v-container>
+    <v-container class="text-center mb-16">
+        <h4 class="text-h5 text-md-h4 mb-2">
+          <b>
+            Find your new friend in your location
+          </b>
+        </h4>
+        <h4 style="color: gray;">
+          choose your favorite pet near your place with our app. And dont forget to rate us
+        </h4>
+    </v-container>
+    <v-container class="text-center">
+        <NuxtLink to="/home">
+          <v-btn class="rounded-lg" color="secondary" style ="text-transform: none;">
+            Masuk
+          </v-btn>
+        </NuxtLink>
+    </v-container>
   </div>
 </template>
+
+<script>
+// import colors from 'vuetify/es5/util/colors';
+
+export default {
+  name: 'IndexPage',
+  mounted(){
+    // this.$store.commit('global/set_path', '/')
+  },  
+  data(){
+    return {
+      items: [
+          {
+            src: '/pic/1.jpg',
+          },
+          {
+            src: '/pic/2.jpg',
+          },
+          {
+            src: '/pic/3.jpg',
+          },
+          {
+            src: '/pic/4.jpg',
+          },
+        ]
+    }
+  }
+}
+</script>
+<style>
+/* .v-btn > .v-btn__content .v-icon {
+  color: #1976d2;
+} */
+
+.v-carousel__controls__item{
+  margin: 0 0px;
+}
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,20 +1,47 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import colors from 'vuetify/lib/util/colors'
+
 export default defineNuxtConfig({
     modules: [
         '@invictus.codes/nuxt-vuetify'
       ],
-      vuetify: {
-        /* vuetify options */
-        vuetifyOptions: {
-          // @TODO: list all vuetify options
-        },
-        moduleOptions: {
-          /* nuxt-vuetify module options */
-          treeshaking: true,
-          useIconCDN: true,
-          /* vite-plugin-vuetify options */
-          styles: true,
-          autoImport: true,
+    vuetify: {
+      /* vuetify options */
+      vuetifyOptions: {
+        theme: {
+          defaultTheme: 'myCustomTheme',
+          themes: {
+            myCustomTheme: {
+              dark: false,
+              variables: {}, // ✅ this property is required to avoid Vuetify crash         
+              colors: {
+                //green: '#00ff00' // cannot use primary color names here, so use a custom color name (such as 'greenish')
+                greenish: '#03DAC5',
+                // Workaround: Custom colors seem to erase default colors, so we need to include the default colors (of `light` or `dark` theme)
+                background: '#ccc',
+                surface: '#212121',
+                primary: colors.orange.darken3, 
+                'primary-darken-1': '#3700B3',
+                secondary: colors.grey.lighten1,
+                'secondary-darken-1': '#03DAC5',
+                error: '#CF6679',
+                info: '#2196F3',
+                success: '#4CAF50',
+                warning: '#FB8C00'
+              },
+            }
+          }
         }
-      }    
+      },
+      moduleOptions: {
+        /* nuxt-vuetify module options */
+        treeshaking: true,
+        useIconCDN: true,
+        /* vite-plugin-vuetify options */
+        styles: true,
+        autoImport: true,
+        
+      },
+      // customVariables: ['~/assets/variables.scss'],
+    }    
 })
